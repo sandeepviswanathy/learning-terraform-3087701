ccd75bf9260595c03a2d2764feb3015c6b5b51bf
Sandeep Viswanath Reddy Yarasani <sandeepviswanathy@gmail.com>
2025-03-23 16:05:04 -0400

    changed traffic_source_attachments alb type kewyword

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ module "blog_ysani_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -49,7 +48,7 @@ module "asg" {
   traffic_source_attachments = {
     ex-alb = {
       traffic_source_identifier = module.alb.target_groups["ex-instance"].arn
-      traffic_source_type       = "elbv2"
+      type       = "elbv2"
     }
   }
 }
