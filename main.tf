0c6d1e06fb17cff58e2ae1482be67b479ea83de4
Sandeep Viswanath Reddy Yarasani <sandeepviswanathy@gmail.com>
2025-03-22 23:33:52 -0400

    indentation inside sg module

--- main.tf
+++ main.tf
@@ -32,13 +32,13 @@ resource "aws_instance" "blog_ysani" {
 module "blog_ysani_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog_ysani_new"
+  name    = "blog_ysani_new"
 
   vpc_id = data.aws_vpc.default.id
   
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
